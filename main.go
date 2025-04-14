c93d7e3c6e41fd42eabb92d478c27a33683b34e4
Pritam Das <pritam.das.santu@gmail.com>
2025-04-14 21:26:02 +0530

    init http server

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main(){
+import (
+	"fmt"
+	"net/http"
+)
 
+func rootHandler(w http.ResponseWriter, req *http.Request){
+	
+
+	fmt.Fprintf(w, "hello\n")
+}
+
+func main(){
+	http.HandleFunc("/", rootHandler)
+
+	http.ListenAndServe(":8080", nil)
 }
